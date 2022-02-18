38ede4674d9bf51c4472a4b874d5ae7a981859b9
Chubbs91 <chaskin1991@outlook.com>
2022-02-17 19:14:00 -0600

    create mainmenu for index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const inquirer = require('inquirer');
+
+function mainMenu() {
+    console.log("Welcome to Employee Tracker!")
+    inquirer.prompt({
+        type: "list",
+        message: "Please choose what you would like to do.",
+        choices: [
+            "View all Employees",
+            "View all Departments",
+            "View all Roles",
+            "Add an Employee",
+            "Add a Department",
+            "Add a Role",
+            "Update Role",
+            "Quit"
+        ],
+    })
+}
+
+mainMenu();
