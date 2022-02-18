e853accc8c83db416dda31ba3684a742e6bdf880
Chubbs91 <chaskin1991@outlook.com>
2022-02-17 19:34:10 -0600

    added else ifs for mainmenu function in index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const orm = require('./config/orm');
 const inquirer = require('inquirer');
 
+// This function generates the top-level choices for the user. Upon selecting any of them, a new function is executed
+// specific to that choice.  Upon completion of the selected task, this function is called again.
 function mainMenu() {
-    console.log("Welcome to Employee Tracker!")
+    console.log("Welcome to Employee Tracker!\n")
     inquirer.prompt({
         type: "list",
         message: "Please choose what you would like to do.",
@@ -15,7 +18,40 @@ function mainMenu() {
             "Update Role",
             "Quit"
         ],
-    })
+        name: "choice"
+    }).then(function({ choice }) {
+        if (choice === "View all Employees") {
+            orm.viewEmployees()
+            .then(function() {
+                console.log("\n");
+                mainMenu();
+            });
+        } else if (choice === "View all Departments") {
+            orm.viewDepartments()
+            .then(function() {
+                console.log("\n");
+                mainMenu();
+            });
+        } else if (choice === "View all Roles") {
+            orm.viewRoles()
+            .then(function() {
+                console.log("\n");
+                mainMenu();
+            });
+        } else if (choice === "Add an Employee") {
+            addEmployeePrompt();
+        } else if (choice === "Add a Department") {
+            addDepartmentPrompt();
+        } else if (choice === "Add a Role") {
+            addRolePrompt();
+        } else if (choice === "Update Role") {
+            updateRolePrompt();
+        } else {
+            orm.endConnection();
+        }
+    });
 }
+
+// note for below
 
 mainMenu();
