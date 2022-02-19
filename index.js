51873ae6ef143a1d56ccd8bffb010268304c7219
Chubbs91 <chaskin1991@outlook.com>
2022-02-19 14:36:37 -0600

    add employee added (index.js)

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const orm = require('./config/orm.js');
 const inquirer = require('inquirer');
 
-// This function generates the top-level choices for the user. Upon selecting any of them, a new function is executed
-// specific to that choice.  Upon completion of the selected task, this function is called again.
+// WHEN I start the application
+// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 function mainMenu() {
     console.log("Welcome to Employee Tracker!\n")
     inquirer.prompt({
@@ -19,25 +19,25 @@ function mainMenu() {
             "Quit"
         ],
         name: "choice"
-    }).then(function({ choice }) {
+    }).then(function ({ choice }) {
         if (choice === "View all Employees") {
             orm.viewEmployees()
-            .then(function() {
-                console.log("\n");
-                mainMenu();
-            });
+                .then(function () {
+                    console.log("\n");
+                    mainMenu();
+                });
         } else if (choice === "View all Departments") {
             orm.viewDepartments()
-            .then(function() {
-                console.log("\n");
-                mainMenu();
-            });
+                .then(function () {
+                    console.log("\n");
+                    mainMenu();
+                });
         } else if (choice === "View all Roles") {
             orm.viewRoles()
-            .then(function() {
-                console.log("\n");
-                mainMenu();
-            });
+                .then(function () {
+                    console.log("\n");
+                    mainMenu();
+                });
         } else if (choice === "Add an Employee") {
             addEmployeePrompt();
         } else if (choice === "Add a Department") {
@@ -52,6 +52,62 @@ function mainMenu() {
     });
 }
 
-// note for below
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+function addEmployeePrompt() {
+    orm.getEmployees()
+        .then(function (res) {
+            const managerArray = [];
+            for (let i = 0; i < res.length; i++) {
+                managerArray.push(res[i].name);
+            }
+            managerArray.push("none");
+            orm.getRoles()
+                .then(function (response) {
+                    const roleTitleArray = [];
+                    for (let i = 0; i < response.length; i++) {
+                        roleTitleArray.push(response[i].title);
+                    }
+                    inquirer.prompt([{
+                        type: "input",
+                        message: "Enter employee's first name",
+                        name: "firstName"
+                    },
+                    {
+                        type: "input",
+                        message: "Enter employee's last name",
+                        name: "lastName"
+                    },
+                    {
+                        type: "list",
+                        message: "Select employee's role",
+                        choices: roleTitleArray,
+                        name: "role"
+                    },
+                    {
+                        type: "list",
+                        message: "Select employee's manager",
+                        choices: managerArray,
+                        name: "manager"
+                    }]).then(function ({ firstName, lastName, role, manager }) {
+                        const roleId = response[roleTitleArray.indexOf(role)].id;
+                        if (manager === "none") {
+                            orm.addEmployee(firstName, lastName, roleId)
+                                .then(function () {
+                                    console.log("\n");
+                                    mainMenu();
+                                });
+                        } else {
+                            const managerId = res[managerArray.indexOf(manager)].id;
+                            orm.addEmployee(firstName, lastName, roleId, managerId)
+                                .then(function () {
+                                    console.log("\n");
+                                    mainMenu();
+                                });
+                        }
+                    });
+                });
+        });
+}
 
 mainMenu();
